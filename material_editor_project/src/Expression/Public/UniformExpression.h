041d7d9fbe18deddd7e195b930dcd7dfcc220711
quan224 <quan224@users.noreply.github.com>
2026-09-02 20:34:21 +0800

    wip(lesson6): MaterialUniformExpressionType registry + DECLARE macro + tree base class (GetType/IsConstant/IsIdentical/GetChildren)

--- material_editor_project/src/Expression/Public/UniformExpression.h
+++ material_editor_project/src/Expression/Public/UniformExpression.h
@@ -1,2 +1,37 @@
 #pragma once
 #include "Core/Public/RefCounted.h"
+#include <map>
+#include <string>
+#include <vector>
+
+
+class MaterialUniformExpressionType{
+public:
+    inline static std::map<std::string, MaterialUniformExpressionType*>& GetTypeMap(){
+        static std::map<std::string, MaterialUniformExpressionType*> type_map;
+        return type_map;
+    }
+
+    MaterialUniformExpressionType(const std::string& n):name(n)
+    {
+        GetTypeMap()[n] = this;
+    }
+
+private:
+    std::string name;
+};
+
+#define DECLARE_MATERIALUNIFROMEXPRESSION_TYPE(Name) \
+    public:\
+    static MaterialUniformExpressionType static_type;
+
+class MaterialUniformExpression{
+public:
+    virtual ~MaterialUniformExpression()=default;
+    virtual MaterialUniformExpressionType* GetType()const =0;
+    virtual bool IsConstant()const{return false;}
+    virtual bool IsIdentical(MaterialUniformExpression* other)const{return false;}
+
+    virtual std::vector<const MaterialUniformExpression* > GetChildren() const {  return std::vector<const MaterialUniformExpression*>(); }
+
+};
